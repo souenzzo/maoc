e138ea004720dff72f81a57fe2b6de0fbc38869e
Enzzo Cavallo <souenzzo@gmail.com>
2024-12-03 07:23:00 -0300

    day3

--- projects/aoc2024/src/aoc2024/main.clj
+++ projects/aoc2024/src/aoc2024/main.clj
@@ -100,3 +100,37 @@
            (parse-day2 example-day2)))
   (count (filter #(safe-line? % 1)
            (parse-day2 (aoc/input 2024 2)))))
+
+
+(def example-day3
+  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
+(def example-day3-2
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+(defn day3
+  [input]
+  (->> (re-seq #"(mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\))" input)
+    (map first)
+    (into [] (fn [rf]
+               (let [*enabled? (volatile! true)]
+                 (fn
+                   ([] (rf))
+                   ([el] (rf el))
+                   ([acc el]
+                    (case (first el)
+                      \d (do (case (count el)
+                               4 (vreset! *enabled? true)
+                               7 (vreset! *enabled? false))
+                             acc)
+                      (if @*enabled?
+                        (rf acc el)
+                        acc)))))))
+    (map (fn [s] (apply * (map parse-long (re-seq #"[0-9]+" s)))))
+    (apply +)))
+
+(comment
+  (day3 example-day3)
+  (day3 (string/join (aoc/input 2024 3)))
+  (day3 example-day3-2))
+
